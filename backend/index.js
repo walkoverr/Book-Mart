d2f2d85e2504499809dcad5fddb3def2598448b1
WALKOVER <abhisathayadav143@gmail.com>
2025-09-16 11:33:35 +0530

    updated cors functionality to allow cross origin access

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,13 @@ const userRoutes = require("./routes/userRoutes.js");
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
